0d1a84426c4c31d11edeeb39b1f9543f597cb9a0
Arpit-Babbar <arpitbabbar@gmail.com>
2023-04-15 12:06:41 +0530

    Minor fixes

--- examples/tree_2d_dgsem/elixir_advection_basic.jl
+++ examples/tree_2d_dgsem/elixir_advection_basic.jl
@@ -10,19 +10,19 @@ equations = LinearScalarAdvectionEquation2D(advection_velocity)
 polydeg = 3
 # Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
 solver = DGSEM(polydeg=polydeg, surface_flux=flux_lax_friedrichs,
-               volume_integral=TrixiLW.VolumeIntegralFR(TrixiLW.MDRK()))
+               volume_integral=TrixiLW.VolumeIntegralFR(TrixiLW.LW()))
 
 coordinates_min = (-1.0, -1.0) # minimum coordinates (min(x), min(y))
 coordinates_max = ( 1.0,  1.0) # maximum coordinates (max(x), max(y))
 
 # Create a uniformly refined mesh with periodic boundaries
 mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=6,
+                initial_refinement_level=5,
                 n_cells_max=30_000) # set maximum capacity of tree data structure
 
 # A semidiscretization collects data structures and functions for the spatial discretization
 semi = TrixiLW.SemidiscretizationHyperbolic(mesh,
-TrixiLW.get_time_discretization(solver), equations,
+time_discretization(solver), equations,
  initial_condition_convergence_test, solver)
 
 
@@ -31,8 +31,8 @@ TrixiLW.get_time_discretization(solver), equations,
 
 # Create ODE problem with time span from 0.0 to 1.0
 # ode = semidiscretize(semi, (0.0, 1.0));
-tspan = (0.0, 10.0)
-lw_update = TrixiLW.semidiscretize(semi, get_time_discretization(solver), tspan);
+tspan = (0.0, 1.0)
+lw_update = TrixiLW.semidiscretize(semi, time_discretization(solver), tspan);
 
 # The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
 analysis_callback = AnalysisCallback(semi, interval=1000)
@@ -52,14 +52,15 @@ callbacks = (;
 
 # OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
 
-time_int_tol = 1e-7
+time_int_tol = 1e-5
 tolerances = (;abstol = time_int_tol, reltol = time_int_tol);
-dt_initial = 1.0
+dt_initial = 1e-3;
 # 0.9 works for 2-staged
-cfl_number = TrixiLW.trixi2lw(0.9, solver)
+cfl_number = TrixiLW.trixi2lw(0.71, solver)
 sol, summary = TrixiLW.solve_lwfr(lw_update, callbacks, dt_initial, tolerances,
-                     #  time_step_computation = TrixiLW.Adaptive(),
-                      time_step_computation = TrixiLW.CFLBased(cfl_number)
+                      time_step_computation = TrixiLW.Adaptive(),
+                     #  time_step_computation = TrixiLW.CFLBased(cfl_number),
+                      stages = TrixiLW.TwoStaged()
                       );
 
 # Print the timer summary
