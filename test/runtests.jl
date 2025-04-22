4697e42da8ddc1dd319d2a70a14882719dd96a80
Arpit Babbar <arpitbabbar@gmail.com>
2025-04-22 03:08:38 +0200

    Lower tolerance again

--- test/runtests.jl
+++ test/runtests.jl
@@ -240,7 +240,7 @@ end
 full_test_name, sol, analysis_callback = @test_trixilw_include("p4est_2d_dgsem",
                                                                "euler_double_mach_reflection_square")
 @testset "$full_test_name" begin
-    compare_errors_txt(sol, analysis_callback, full_test_name)
+    compare_errors_txt(sol, analysis_callback, full_test_name; tol = 1e-7)
 end
 
 full_test_name, sol, analysis_callback = @test_trixilw_include("p4est_2d_dgsem",
