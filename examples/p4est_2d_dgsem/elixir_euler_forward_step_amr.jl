be0ab897b3ad21d0fbf95ad4b75e76e876a47585
Arpit Babbar <arpitbabbar@gmail.com>
2025-04-22 03:22:19 +0200

    A dependency fix

--- examples/p4est_2d_dgsem/elixir_euler_forward_step_amr.jl
+++ examples/p4est_2d_dgsem/elixir_euler_forward_step_amr.jl
@@ -2,7 +2,7 @@
 using Downloads: download
 using TrixiLW
 using TrixiLW.Trixi
-using LinearAlgebra
+using TrixiLW.Trixi.LinearAlgebra
 
 ###############################################################################
 # semidiscretization of the compressible Euler equations
