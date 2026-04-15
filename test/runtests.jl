6454f3af433a438dc0779c0d841c31ef3cd98ec0
Arpit Babbar <arpitbabbar@gmail.com>
2026-04-15 18:27:30 +0200

    Update tolerance

--- test/runtests.jl
+++ test/runtests.jl
@@ -127,7 +127,7 @@ end
 full_test_name, sol, analysis_callback = @test_trixilw_include("tree_2d_dgsem",
                                                                "euler_double_mach_reflection_square")
 @testset "$full_test_name" begin
-    compare_errors_txt(sol, analysis_callback, full_test_name)
+    compare_errors_txt(sol, analysis_callback, full_test_name, tol = 1e-12)
 end
 
 full_test_name, sol, analysis_callback = @test_trixilw_include("tree_2d_dgsem",
